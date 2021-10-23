ee7580283d36fffa5941a03df5fe842842920fec
akshaygoel1 <goel.akshay1@gmail.com>
2021-10-23 12:18:27 +0530

    Init

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float runSpeed = 10f;
+    public GameObject player;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.position = new Vector3(player.transform.position.x + speed*Time.deltaTime, player.transform.position.y, player.transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.transform.position = new Vector3(player.transform.position.x - speed * Time.deltaTime, player.transform.position.y, player.transform.position.z);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Compartment.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compartment : MonoBehaviour
+{
+    public SpriteRenderer topLayer;
+    bool isInCompartment = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Fader(-1));
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Fader(1));
+        }
+    }
+
+    IEnumerator Fader(int multiplier)
+    {
+        if(multiplier == 1)
+        {
+            while(!Mathf.Approximately(topLayer.color.a,1f))
+            {
+                yield return new WaitForSeconds(0.01f);
+                topLayer.color = new Color(topLayer.color.r, topLayer.color.g, topLayer.color.b, topLayer.color.a + 0.1f);
+                if (topLayer.color.a > 0.95f)
+                {
+                    topLayer.color = new Color(topLayer.color.r, topLayer.color.g, topLayer.color.b, 1f);
+                    yield break;
+                }
+            }
+        }
+        else
+        {
+            while (!Mathf.Approximately(topLayer.color.a, 0f))
+            {
+                yield return new WaitForSeconds(0.01f);
+                topLayer.color = new Color(topLayer.color.r, topLayer.color.g, topLayer.color.b, topLayer.color.a - 0.1f);
+                if (topLayer.color.a < 0.1f)
+                {
+                    topLayer.color = new Color(topLayer.color.r, topLayer.color.g, topLayer.color.b, 0f);
+                    yield break;
+                }
+            }
+        }
+    }
+}
